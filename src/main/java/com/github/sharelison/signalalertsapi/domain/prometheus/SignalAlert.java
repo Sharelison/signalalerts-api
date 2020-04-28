2fd792cc1c37c56f44a8a33c4d81830d4a15195f
sharelison <sharelison@hotmail.com>
2020-04-28 10:59:40 +0200

    if not null

--- src/main/java/com/github/sharelison/signalalertsapi/domain/prometheus/SignalAlert.java
+++ src/main/java/com/github/sharelison/signalalertsapi/domain/prometheus/SignalAlert.java
@@ -25,16 +25,18 @@ public class SignalAlert {
     public String toSignalAlertMessage() {
         StringBuilder alertMessageSb = new StringBuilder("ALERT\n\n");
 
-        alertmanageralert.getAlerts().forEach(a -> {
-            alertMessageSb.append("Name: ").append(a.getLabels().getAlertname())
-                          .append("\nCluster: ").append(a.getLabels().getCluster())
-                          .append("\nStatus: ").append(a.getStatus())
-                          .append("\nSeverity: ").append(a.getLabels().getSeverity())
-                          .append("\nStarts at: ").append(a.getStartsAt())
-                          .append("\nEnds at: ").append(a.getEndsAt())
-                          .append("\nDescription: ").append(a.getAnnotations().getDescription())
-                          .append("\n\n");
-        });
+        if(alertmanageralert != null) {
+            alertmanageralert.getAlerts().forEach(a -> {
+                alertMessageSb.append("Name: ").append(a.getLabels().getAlertname())
+                        .append("\nCluster: ").append(a.getLabels().getCluster())
+                        .append("\nStatus: ").append(a.getStatus())
+                        .append("\nSeverity: ").append(a.getLabels().getSeverity())
+                        .append("\nStarts at: ").append(a.getStartsAt())
+                        .append("\nEnds at: ").append(a.getEndsAt())
+                        .append("\nDescription: ").append(a.getAnnotations().getDescription())
+                        .append("\n\n");
+            });
+        }
 
         return alertMessageSb.toString();
     }
